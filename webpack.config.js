1dd1fe8ba18dc71662c99c7dce12ce8698818438
Santtu Lintervo <santervo@gmail.com>
2016-10-24 12:32:33 +0300

    NPM build configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var path = require("path");
 module.exports = {
   entry: {
-    app: ["./example/example.js"]
+    app: ["./src/index.js"]
   },
   module: {
     loaders: [{
@@ -13,9 +13,30 @@ module.exports = {
       }
     }],
   },
+  externals: [
+    {
+      jquery: {
+        commonjs: 'jquery',
+        commonjs2: 'jquery',
+        amd: 'jquery',
+        root: 'jQuery'
+      }
+    },
+    {
+      hammerjs: {
+        commonjs: 'hammerjs',
+        commonjs2: 'hammerjs',
+        amd: 'hammerjs',
+        root: 'Hammer'
+      }
+    },
+    'jquery.transit'
+  ],
   output: {
+    library: 'DragView',
+    libraryTarget: 'umd',
+    path: path.resolve(__dirname, 'lib'),
     publicPath: "/build/",
-    filename: "bundle.js"
-  },
-  devtool: 'source-map'
+    filename: "drag-view.js"
+  }
 };
