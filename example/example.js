fbf44b56f2cfeadd7d5359b39753b8d714eb8702
Santtu Lintervo <santervo@gmail.com>
2016-10-26 14:18:42 +0300

    Set transform origin always on start of scaling

--- example/example.js
+++ example/example.js
@@ -2,4 +2,4 @@ import DragView from '../src'
 
 var el = document.getElementById('container')
 
-new DragView(el, { scaleable: true })
+new DragView(el, { scaleable: true, minScale: 0.5, maxScale: 3 })
--- src/DragView.js
+++ src/DragView.js
@@ -28,12 +28,12 @@ export default class DragView {
 
     // Enable scaling optionally
     if(opts.scaleable) {
-      // Set transform origin so full scaled content will be scaleable
-      this.el.children().css({transformOrigin: "0 0"})
       this.hammer.get('pinch').set({ enable: true })
       this.hammer.on("pinchstart", this.onPinchStart.bind(this))
       this.hammer.on("pinch", this.onPinch.bind(this))
     }
+    this.setTranslate({x: 0, y: 0})
+    this.setScale(1)
   }
 
   // Function that stores the scroll position at the start of pan event
@@ -49,17 +49,49 @@ export default class DragView {
     this.setScrollPosition(position)
   }
 
-  onPinchStart() {
+  onPinchStart(event) {
+    // Record start value of scale
     this.scaleStart = this.getScale()
+
+    // Update transform origin to center point of pinch
+    const transformOrigin = {
+      x: (event.center.x - event.target.x) / this.scaleStart,
+      y: (event.center.y - event.target.y) / this.scaleStart
+    }
+
+    this.updateTransformOrigin(transformOrigin)
   }
 
   onPinch(event) {
     var scale = event.scale
     scale = scale * this.scaleStart
     this.scaleElement(scale)
+    this.adjustPosition()
+  }
 
-    // Update scroll position so that we don't go over boundary
-    this.setScrollPosition(this.getScrollPosition())
+  updateTransformOrigin(transformOrigin) {
+    this.setTransformOrigin(transformOrigin)
+
+    const translationOffset = this.calculateTranslationOffset()
+    this.setTranslate(translationOffset)
+  }
+
+  adjustPosition() {
+    const translationOffset = this.calculateTranslationOffset()
+    const adjustedScrollPosition = this.calculateAdjustedScrollPosition(translationOffset)
+
+    this.setTranslate(translationOffset)
+    this.setScrollPosition(adjustedScrollPosition)
+  }
+
+  calculateAdjustedScrollPosition(translationOffset) {
+    const translate = this.getTranslate()
+    const scrollPosition = this.getScrollPosition()
+
+    return  {
+      x: scrollPosition.x + (translationOffset.x - translate.x),
+      y: scrollPosition.y + (translationOffset.y - translate.y)
+    }
   }
 
   scaleElement(scale) {
@@ -71,6 +103,53 @@ export default class DragView {
   // get/set scale
   getScale() {
     return this.el.children().css('scale')
+  }
+
+  // Calculates how much translate we need to position content in (0,0) inside parent element
+  calculateTranslationOffset() {
+    var scale = this.getScale()
+    var origin = this.getTransformOrigin()
+    return {
+      x: origin.x * (scale-1),
+      y: origin.y * (scale-1)
+    }
+  }
+
+  // Return current {x,y} transform origin
+  getTransformOrigin() {
+    var transformOrigin = this.el.children().css('transformOrigin')
+    var parts = transformOrigin.split(" ")
+    return {
+      x: parseInt(parts[0]),
+      y: parseInt(parts[1])
+    }
+  }
+
+  setTransformOrigin(transformOrigin) {
+    const transformOriginStr = transformOrigin.x + 'px ' + transformOrigin.y + 'px'
+    this.el.children().css({transformOrigin: transformOriginStr})
+  }
+
+  // Return current {x,y} translate
+  getTranslate() {
+    var translate = this.el.children().css('translate');
+    var parts
+
+    if(typeof(translate) === 'string') {
+      // Parse translate string "-123px,-61px"
+      parts = translate.split(',')
+      return {
+        x: parseInt(parts[0]),
+        y: parseInt(parts[1])
+      }
+    }
+    else {
+      return { x:0, y: 0}
+    }
+  }
+
+  setTranslate(translation) {
+    this.el.children().css({translate: [translation.x, translation.y]})
   }
 
   setScale(scale) {
